544734a8583954fe46ab37c602abbe7ad31cfa77
K1ngVlad <kingvlad3008@gmail.com>
2025-10-08 16:47:56 +0400

    add typescript

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// import { ModbusMonitoring } from './modbus-monitoring.js';
+
+// const ADRESS = '127.0.0.1';
+// const PORT = 502;
+
+// const TAGS_COUNT = 10;
+// const METERS = [1, 2];
+// const INTERVAL = 1000;
+
+// const modbusMonitoring = new ModbusMonitoring(ADRESS, { port: PORT });
+
+// modbusMonitoring
+//   .setTagsCount(TAGS_COUNT)
+//   .setMeters(METERS)
+//   .setInterval(INTERVAL)
+//   .start();
+
+console.log('okak');
